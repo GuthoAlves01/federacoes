9a3addc49124c2b70141b7b9f28e1802a7a7994a
guthemberg alves <guthorodrigues2009@gmail.com>
2025-05-02 21:42:35 -0300

    commit inicial

---
+++ src/main/java/br/gthg/federacao/FederacaoApplication.java
@@ -0,0 +1,13 @@
+package br.gthg.federacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FederacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gthg/federacao/FederacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gthg.federacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
